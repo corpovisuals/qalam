ea40d770309b0fd70ccb38f0fb02b0248df34c71
mazharulmaruf228 <mazharul.maruf228@gmail.com>
2020-08-08 22:10:59 +0600

    Add close modal dialogue button

--- app/components/image-gallery/index.js
+++ app/components/image-gallery/index.js
@@ -1,7 +1,13 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ImageGallery extends Component {
   page = 1
   perPage = 4
+
+  @action
+  closeScreen() {
+    this.args.close();
+  }
 }
